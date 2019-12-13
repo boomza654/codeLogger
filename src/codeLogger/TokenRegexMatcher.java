ed0b967f68e8ccef025ab3920dfab9359b2f1376
Boomza654 <krittamate.t@gmail.com>
2019-12-12 19:00:17 -0500

    Add more Regex Type

--- src/codeLogger/TokenRegexMatcher.java
+++ src/codeLogger/TokenRegexMatcher.java
@@ -17,9 +17,20 @@ public class TokenRegexMatcher {
  *
  */
 interface TRgx{
-    
+    public static TRgx empty() { return new TRgxEmpty();}
+    public static TRgx token(TokenType t) { return new TRgxToken(t);}
+    public static TRgx tSet(Set<TokenType> t) { return new TRgxTokenSet(t);}
+    public static TRgx tRgxQM(TRgx t) { return new TRgxOr(new TRgxEmpty(), t);}
+    public static TRgx tRgxStar(TRgx t) { return new TRgxRepeat(t);}
+    public static TRgx tRgxPlus(TRgx t) { return new TRgxConcat(t,tRgxStar(t));}
+    public static TRgx tRgxPipe(TRgx ...t1) { return new TRgxOr(Arrays.asList(t1));}
+    public static TRgx tRgxConcat(TRgx ...t1) { return new TRgxConcat(Arrays.asList(t1));}
+       
 }
 
+class TRgxEmpty implements TRgx{
+    
+}
 class TRgxToken implements TRgx{
     private final TokenType type;
     TRgxToken(TokenType t){ type=t;}
@@ -32,11 +43,14 @@ class TRgxTokenSet implements TRgx{
 }
 class TRgxConcat implements TRgx{
     private final List<TRgx> exprs;
+
+    TRgxConcat(TRgx t1, TRgx t2){ this(List.of(t1,t2));}
     TRgxConcat(List<TRgx> t){ exprs= List.copyOf(t);}
     List<TRgx> getExprList(){ return List.copyOf(exprs);}
 }
 class TRgxOr implements TRgx{
     private final List<TRgx> exprs;
+    TRgxOr(TRgx t1, TRgx t2){ this(List.of(t1,t2));}
     TRgxOr(List<TRgx> t){ exprs= List.copyOf(t);}
     List<TRgx> getExprList(){ return List.copyOf(exprs);}
 }
