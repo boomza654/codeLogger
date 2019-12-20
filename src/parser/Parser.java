91487b007e9f4a42f0f7084df47cf08595082e82
Boomza654 <krittamate.t@gmail.com>
2019-12-20 13:17:18 -0500

    Add real parser class + test file

---
+++ src/parser/Parser.java
@@ -0,0 +1,17 @@
+package parser;
+
+
+/**
+ * Real parser to parse things from  antlr4 API
+ * @author boomza654
+ *
+ */
+public class Parser {
+    /**
+     * 
+     * @param args file name of source file to parse ( right now still have no effect)
+     */
+    public static void main(String[] args) {
+        
+    }
+}
